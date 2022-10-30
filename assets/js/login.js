34065b1119c6464632525785bd156e3798647c4e
Alex Correa <acorreamonroy@gmail.com>
2022-10-30 01:46:44 -0500

    Muchachos he realizado la creación de una nueva rama en GitHub y tabién realicé la modificación de la palabra Afrojohan que antes aparecía como 'Afrojahan'.

---
+++ assets/js/login.js
@@ -0,0 +1,5 @@
+//Probando consola
+lista = new Array('a', 'b', 's', 'e', 'n', 'a', 'g', 'h');
+sublista = lista.slice(2,6);
+console.log(sublista.join());
+console.log(sublista[0]+sublista[1]+sublista[2]+sublista[3]);
